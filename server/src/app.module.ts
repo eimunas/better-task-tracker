e5e48984d9bda53de5f86a549852998782873226
eimunas <88548016+eimunas@users.noreply.github.com>
2026-01-20 20:42:46 +0100

    Add postgres database image, dockerize server and database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres_db',
+      port: 5432,
+      database: 'better-task-tracker',
+      username: 'admin',
+      password: 'password',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
